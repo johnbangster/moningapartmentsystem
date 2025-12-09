b4795172c01b94a48808fb9cb67bac962e2ada88
JanJan <34339416+johnbangster@users.noreply.github.com>
2025-12-09 17:02:00 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,17 +1,16 @@
 <?php
 session_start();
 
-    if(isset($_POST['logout']))
-    {
+if(isset($_POST['logout_btn']))
+{
 
-        //session destroy
-        unset($_SESSION['auth']);
-        unset($_SESSION['auth_role']);
-        unset($_SESSION['auth_user']);
+    //session destroy
+    unset($_SESSION['auth']);
+    unset($_SESSION['auth_role']);
+    unset($_SESSION['auth_user']);
 
-        $_SESSION['message'] = "Logged out successfully";
-        header("Location: /login.php");
-        exit(0);
-
-    }
+    $_SESSION['message'] = "Logged out successfully";
+    header("Location: https://moningsrentalservice.com/login.php");
+    exit;
+}
 ?>
